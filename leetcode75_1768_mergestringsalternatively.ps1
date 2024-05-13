a4733bfabe3ea26f016ca576dae0d0473e2add44
David Sachs <sachs.davidr@gmail.com>
2024-05-13 14:31:20 -0500

    add leetcode problem 1768 posh answer

---
+++ leetcode75_1768_mergestringsalternatively.ps1
@@ -0,0 +1,16 @@
+﻿#https://leetcode.com/problems/merge-strings-alternately/description/
+#took about 20 minutes...used snippets as well as get-member
+$word1 = "abcd"; $word2 = "pq"
+
+$word1.ToCharArray()
+$word2.toCharArray()
+
+ $longestStringLength = $(($word1.Length, $word2.Length) | Measure-Object -Maximum).Maximum
+
+ $mergedWord = ""
+for ($i = 0; $i -lt $($longestStringLength);$i++) {
+    $mergedWord += $word1[$i]
+    $mergedWord += $word2[$i]
+}
+
+$mergedWord
