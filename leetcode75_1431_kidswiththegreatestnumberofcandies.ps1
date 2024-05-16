cb952513fa8eb1f63f85e9d3de05f0b9b7fc8ac2
David Sachs <sachs.davidr@gmail.com>
2024-05-16 17:40:50 -0500

    add another leetcode answer

---
+++ leetcode75_1431_kidswiththegreatestnumberofcandies.ps1
@@ -0,0 +1,17 @@
+﻿#https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/?envType=study-plan-v2&envId=leetcode-75
+#1431 Kids With the Greatest Number of Candies
+$candies = @(12,1,12)
+$extraCandies = 10
+
+$kidWithMostCandiesCurrently = $($candies | Measure-Object -Maximum).Maximum
+
+$result = $null 
+$candies | % {
+    if ($($_ + $extraCandies) -ge $kidWithMostCandiesCurrently){
+        $result += @(1)
+    }
+    else {
+        $result += @(0)
+    }
+}
+$result
