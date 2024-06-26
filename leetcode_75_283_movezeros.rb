73405773715a23a3d054442e749fbc2bd2b3f95b
David Sachs <sachs.davidr@gmail.com>
2024-06-26 18:32:11 -0500

    first ruby leetcode

---
+++ leetcode_75_283_movezeros.rb
@@ -0,0 +1,14 @@
+nums = [0,1,0,3,12]
+newnums = []
+nums.each do |num|
+    unless num == 0
+        newnums << num
+    end
+end
+
+(1..nums.length - newnums.length).each do
+    newnums << 0
+end
+
+puts newnums.inspect
+    
